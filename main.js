ec8840a000bbd58ee230c3335c7b20d11de28e60
Esio Almeida <122577592+esioalmeida@users.noreply.github.com>
2023-09-04 19:54:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+//Variáveis
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+let randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+//Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handResetclick)
+document.addEventListener('keydwon', function(e) {
+  if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+    handResetclick()
+  }
+})
+
+//Funções
+function handleTryClick(event) {
+  event.preventDefault()
+
+  const inputNumber = document.querySelector("#inputNumber")
+  
+  if(Number(inputNumber.value) == randomNumber) {
+    toggleScreen()
+    screen2.querySelector("h2").innerText = `acertou em ${xAttempts} tentativas`
+  }
+
+  inputNumber.value = ""
+  xAttempts++
+}
+
+function handResetclick() {
+    toggleScreen()
+    xAttempts = 1
+    randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
